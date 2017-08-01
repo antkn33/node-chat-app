cee7fc1e1f63d43651e4cbec834dcfdb694d5032
Anthony Scott <antkn33@gmail.com>
2017-08-01 12:56:01 -0400

    emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
 	console.log('Connected to server');
-
-	socket.emit('createMessage', {
-		from: 'antknee@gmail.com',
-		text: 'hey there'
-	});
 });
 
 socket.on('disconnect', function () {
